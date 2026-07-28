28702df723482ba09265b8e8164696122bde46de
Rishu Kumar <rish093100@gmail.com>
2026-07-28 18:53:15 +0530

    Day 2 - Conversion Operations

---
+++ Day-01/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score) //isse woh score number me convert ho jayega
+console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+// ************************ Operations *****************************
+
+let value = 3
+let negValue = -value //convert it to negative value
+console.log(negValue);
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+ 
