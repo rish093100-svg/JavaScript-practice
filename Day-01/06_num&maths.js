61f7e8927f16743dceda2fdb1ba38066a54d22a2
Rishu Kumar <rish093100@gmail.com>
2026-07-31 19:04:14 +0530

    Day 2 - Arrays

--- Day-01/06_num&maths.js
+++ Day-01/06_num&maths.js
@@ -2,10 +2,20 @@ const score = 400
 console.log(score); //400
 
 const balance = new Number(100)
-console.log(balance);
+console.log(balance); // [Number: 100]
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));
+console.log(balance.toString().length); // 3
+console.log(balance.toFixed(2)); // 100.00
 
-const otherNumber = 123.8966
-console.log(otherNumber.toPrecision(3));
+const otherNumber = 123.8966 // 
+console.log(otherNumber.toPrecision(2)); //124
+
+// ++++++++++++++++ Maths ++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4)); // 4
+console.log(Math.round(4.6)); // 4
+console.log(Math.ceil(4.2)); // 5
+console.log(Math.floor(4.9)); // 4
+console.log(Math.min(4,3,5,8)); // 2
+console.log(Math.max(4.1,2,3)); // 4
