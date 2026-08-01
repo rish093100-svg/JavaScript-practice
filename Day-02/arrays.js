5e1cf6ec87bf61eb7b0b10e694d77c2c743826fe
Rishu Kumar <rish093100@gmail.com>
2026-08-01 19:38:48 +0530

    Day 6 - Objects

--- Day-02/arrays.js
+++ Day-02/arrays.js
@@ -8,33 +8,33 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 // Array methods
 
-// myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
+myArr.push(6) // [0,1,2,3,4,5,6]
+myArr.push(7) // [0,1,2,3,4,5,6,7]
+myArr.pop() //last value delete ho jayegi
 
-// myArr.unshift(9)
-// myArr.shift()
+myArr.unshift(9) //ye 9 ko starting me kar dega
+myArr.shift() // ye starting ki value ko hata dega 
 
 // console.log(myArr.includes(9));
 // console.log(myArr.indexOf(3));
 
-// const newArr = myArr.join()
+const newArr = myArr.join() //ye array ki type ko change kar deta hai
 
-// console.log(myArr);
-// console.log( newArr);
+console.log(myArr);
+console.log(newArr);
 
 
 // slice, splice
 
-console.log("A ", myArr);
+console.log("A ", myArr); // A [0,1,2,3,4,5,]
 
-const myn1 = myArr.slice(1, 3)
+const myn1 = myArr.slice(1, 3) // start from 1 and stop at before 3 => [1,2]
+console.log(myn1); // [1,2]
 
-console.log(myn1);
-console.log("B ", myArr);
+console.log("B ", myArr); // B [0,1,2,3,4,5,]
 
 
 const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
-console.log(myn2);
+console.log("C ", myArr); // C [0,4,5]
+console.log(myn2); // [1,2,3]
 
