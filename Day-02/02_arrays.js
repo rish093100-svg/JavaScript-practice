2fcdd1e2eb94a79fc953b992c35d408b61da0fe2
Rishu Kumar <rish093100@gmail.com>
2026-08-05 21:21:06 +0530

    Day 9 - arrow

--- Day-02/02_arrays.js
+++ Day-02/02_arrays.js
@@ -20,12 +20,12 @@ console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.isArray("Hitesh")) // false
+console.log(Array.from("Hitesh")) //ye array bna dega [ 'H', 'i', 't', 'e', 's', 'h' ]
+console.log(Array.from({name: "hitesh"})) // ye empty array dega
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3)); // returns new array from set of elements
